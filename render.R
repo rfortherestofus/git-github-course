df3e8eef1c07675ee326f397e6c05118b0b89525
David Keyes <david@rfortherestofus.com>
2022-01-13 11:24:39 -0800

    Render updated slides

---
+++ render.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(pagedown)
+library(rmarkdown)
+
+
+# Render slides -----------------------------------------------------------
+
+render("slides.Rmd")
+chrome_print("slides.html", "slides.pdf")
+
+
+# Create GIF of slides ----------------------------------------------------
+
+pdf_to_gif <- function(pdf_location,
+                       gif_file_path = here::here(),
+                       gif_file_name,
+                       density = 50) {
+  
+  pdf_doc <- magick::image_read_pdf(pdf_location,
+                                    density = 50)
+  
+  gif_file_name_and_path <- stringr::str_glue("{gif_file_path}/{gif_file_name}")
+  
+  pdf_doc %>%
+    magick::image_animate(fps = 0.5) %>%
+    magick::image_write(path = gif_file_name_and_path)
+  
+}
+
+
+pdf_to_gif("slides.pdf",  gif_file_name = "slides")
